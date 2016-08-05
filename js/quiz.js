028f60eeecfc20783acf6ab43d2e3dfe17ee1a01
dhaivat <dhaivat.dharsandiya@mavs.uta.edu>
2016-08-05 11:12:13 -0500

    commit

---
+++ js/quiz.js
@@ -0,0 +1,51 @@
+(function(){
+
+	var app = angular.module('myQuiz',[]);
+
+
+	app.controller('QuizController',['$scope','$http','$sce',function($scope,$http,$sce){
+
+		$scope.score=0;
+		$scope.activeQuestion = -1;
+		$scope.activeQuestionAnswered = 0;
+		$scope.percentage = 0; 
+
+		$http.get('quiz_data.json').then(function(quizData){
+
+			console.log(quizData.data);
+			$scope.myQuestions = quizData.data;
+
+			$scope.totalQuestions = $scope.myQuestions.length;
+
+
+
+		});
+		
+		$scope.selectAnswer = function(qIndex,aIndex){
+
+			var questionState = $scope.myQuestions[qIndex].questionState;
+			if (questionState != 'answered')
+			{
+				$scope.myQuestions[qIndex].selectAnswer = aIndex;
+				var correctAnswer = $scope.myQuestions[qIndex].correct;
+
+				if (questionState == correctAnswer)
+				{
+					$scope.myQuestions[qIndex].correctness = "correct";
+					$scope.score += 1;
+
+				}
+				else
+				{
+					$scope.myQuestions[qIndex].correctness="incorrect";
+				}
+
+				$scope.myQuestions[qIndex].questionState ="answered";
+			}
+		}
+
+	}]);
+
+
+
+})();
